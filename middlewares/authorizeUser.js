2032f3d4308e3cc40b2d032ceb19a24992a54424
claurennt <c.benedetti1989@gmail.com>
2022-03-12 00:07:44 +0100

    refactor authorization handler

--- middlewares/authorizeUser.js
+++ middlewares/authorizeUser.js
@@ -1,20 +1,33 @@
 const jwt = require("jsonwebtoken");
-const { JWT_SECRET_KEY, JWT_ADMIN_KEY } = process.env;
 const atob = require("atob");
 
+const { JWT_SECRET_KEY, JWT_ADMIN_KEY } = process.env;
+
+//authorize user after login
 const authorizeUser = (req, res, next) => {
   const authHeader = req.headers.authorization;
 
   if (!authHeader)
     return res.status(401).send("Access denied. No token provided.");
 
-  const [_, authToken] = authHeader.split(" ");
-  // const regex=/^role"role":
-  //   console.log(Buffer.from(authToken, "base64").toString());
-  try {
-    const payload = jwt.verify(authToken, JWT_ADMIN_KEY);
+  const [, authToken] = authHeader.split(" ");
 
-    res.send(payload);
+  const payload = authToken.split(".")[1];
+
+  //decode base64 encoded payload to string first and then parse it to JSON
+  const decodedUserData = JSON.parse(atob(payload));
+
+  //get role for role-based jwt verification
+  const role = decodedUserData.role;
+
+  //verify the token against the key depending on the role decoded from the token
+  try {
+    const userContext = jwt.verify(
+      authToken,
+      role === "admin" ? JWT_ADMIN_KEY : JWT_SECRET_KEY
+    );
+
+    return res.status(200).send(userContext);
   } catch (e) {
     next(e);
   }
